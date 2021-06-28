a66079534089cb357afcadc27c3c942a88fdf2cd
tsm55555 <57450036+tsm55555@users.noreply.github.com>
2021-06-28 20:41:27 +0800

    Add files via upload

--- gem5/configs/common/Caches.py
+++ gem5/configs/common/Caches.py
@@ -81,6 +81,7 @@ class L3Cache(Cache):
     mshrs = 20
     tgts_per_mshr = 24
     write_buffers = 16
+    #replacement_policy = Param.BaseReplacementPolicy(BRRIPRP(),"Replacement policy")#rrip
 
 class IOCache(Cache):
     assoc = 8
